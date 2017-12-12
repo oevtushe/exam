05134b377eaf7525c3207e2f40f552a0c91b180b
Oleksandr EVTUSHENKO <oevtushe@e3r1p6.unit.ua>
2017-12-12 09:11:41 +0200

    rostring

--- rostring.c
+++ rostring.c
@@ -6,28 +6,34 @@
 /*   By: oevtushe <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/12/12 07:12:00 by oevtushe          #+#    #+#             */
-/*   Updated: 2017/12/12 07:57:11 by oevtushe         ###   ########.fr       */
+/*   Updated: 2017/12/12 09:10:16 by oevtushe         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stdlib.h>
 #include <unistd.h>
+#include <stdio.h>
 
-int		ft_strlen(char *str)
+int		ft_strlen_spec(char *str)
 {
 	int len;
 
 	len = 0;
-	while (*str++)
-		++len;
+	while (*str)
+	{
+		if (*str == ' ' || *str == '\t')
+		{
+			++len;
+			while (*str == ' ' || *str == '\t')
+				str++;
+		}
+		else
+		{
+			++len;
+			str++;
+		}
+	}
 	return (len);
-}
-
-void	ft_putstr(char *str)
-{
-	if (str)
-		while (*str)
-			write(1, str++, 1);
 }
 
 int		ft_is_ws(char c)
@@ -35,6 +41,24 @@ int		ft_is_ws(char c)
 	if (c != ' ' && c != '\t' && c != '\0')
 		return (1);
 	return (0);
+}
+
+void	init_middle(char *str, char *res, int *i, int *j)
+{
+	while (str[*i])
+	{
+		if (str[*i] == ' ' || str[*i] == '\t')
+		{
+			res[(*j)++] = str[(*i)++];
+			while (str[*i] == ' ' || str[*i] == '\t')
+			{
+				++(*i);
+			}
+		}
+		else
+			res[(*j)++] = str[(*i)++];
+
+	}
 }
 
 char	*rostring(char *str)
@@ -55,10 +79,10 @@ char	*rostring(char *str)
 	end = i;
 	while (str[i] == ' ' || str[i] == '\t')
 		++i;
-	res = (char*)malloc(sizeof(char) * (ft_strlen(&str[i]) + (end - start) + 2));
-	while (str[i])
-		res[j++] = str[i++];
-	res[j++] = ' ';
+	res = (char*)malloc(sizeof(char) * (ft_strlen_spec(&str[i]) + (end - start) + 2));
+	init_middle(str, res, &i, &j);
+	if (j && (str[start] != ' ' || str[start] != '\t'))
+		res[j++] = ' ';
 	while (start < end)
 		res[j++] = str[start++];
 	res[j] = '\0';
@@ -67,12 +91,18 @@ char	*rostring(char *str)
 
 int		main(int argc, char **argv)
 {
+	char	c;
+	char	*str;
+
+	c = '\n';
 	if (argc != 2)
 	{
-		ft_putstr("\n");
+		write(1, &c, 1);
 		return (1);
 	}
-	ft_putstr(rostring(argv[1]));
-	ft_putstr("\n");
+	str = rostring(argv[1]);
+	while (*str)
+		write(1, str++, 1);
+	write(1, &c, 1);
 	return (0);
 }
