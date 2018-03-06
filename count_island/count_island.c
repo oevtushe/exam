392496d4a3672f1722703c1081d3f84cf45ed68a
oevtushe <casio6290@gmail.com>
2018-03-06 12:08:42 +0200

    Recursive algorithm in use

--- count_island/count_island.c
+++ count_island/count_island.c
@@ -3,6 +3,45 @@
 #include <stdio.h>
 #include "island.h"
 
+void	count_island_hlp(char **map, int i, int j, int filler)
+{
+	if (i >= 0 && j >= 0 && map[i] && map[i][j])
+	{
+		if (map[i][j] == 'X')
+		{
+			map[i][j] = filler;
+			count_island_hlp(map, i, j + 1, filler);
+			count_island_hlp(map, i, j - 1, filler);
+			count_island_hlp(map, i + 1, j, filler);
+			count_island_hlp(map, i - 1, j, filler);
+		}
+	}
+}
+
+void	count_island(char **map)
+{
+	int		i;
+	int		j;
+	char	filler;
+
+	i = 0;
+	filler = '1';
+	while (map[i])
+	{
+		j = 0;
+		while (map[i][j])
+		{
+			if (map[i][j] == 'X')
+			{
+				count_island_hlp(map, i, j, filler);
+				++filler;
+			}
+			++j;
+		}
+		++i;
+	}
+}
+
 int		main(int argc, char **argv)
 {
 	int fd;
@@ -14,6 +53,11 @@ int		main(int argc, char **argv)
 	cnt = 0;
 	fd = open(argv[1], O_RDONLY);
 	map = read_map(fd);
+	while (map[cnt])
+		printf("%s\n", map[cnt++]);
+	count_island(map);
+	cnt = 0;
+	printf("\n\n");
 	while (map[cnt])
 		printf("%s\n", map[cnt++]);
 	free_ptr_arr((void ***)&map);
